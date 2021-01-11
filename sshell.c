88cdd0c6908e92368cf83c88deabf3fd177aa1a5
Tomi-eng <faakeju@ucdavis.edu>
2021-01-10 18:42:42 -0800

    Initial commit

--- sshell.c
+++ sshell.c
@@ -4,12 +4,49 @@
 #include <unistd.h> 
 #include <sys/wait.h>
 
-#define CMDLINE_MAX 512
+#define CMDLINE_MAX 512 
+
+struct command
+{  char input[CMDLINE_MAX];
+   char *cmd; 
+   char *args[17]; 
+};
+
+
+
+void parse_arg(struct command *obj,char *str){
+  char delim[] = " "; 
+  int count = 1;  
+  char *ptr= strtok(NULL, delim); 
+  obj->args[0]= obj->cmd;
+  obj->args[1] = ""; 
+  
+  if(ptr != NULL){ 
+	obj->args[1] = ptr ;
+	while((ptr != NULL) && (count < 16)) { 
+		ptr = strtok(NULL, delim); 
+		count = count + 1;
+		obj->args[count] = ptr ;
+ 	} 
+      }	
+	obj->args[count+1] = NULL;    
+}
+
+void parse_cmd(struct command *obj,char *str){ 
+	char delim[] = " "; 
+	obj->cmd= strtok(str, delim); 
+	parse_arg(obj,str);
+}
+
+
 
 int main(void)
 {
-        char cmd[CMDLINE_MAX]; 
-	char *args[] = {"", NULL};
+        /*char cmd[CMDLINE_MAX]; 
+	char *args[] = {"", NULL};*/ 
+
+	struct command x1; 
+        char line[CMDLINE_MAX];	
 
         while (1) {
                 char *nl;
@@ -21,33 +58,35 @@ int main(void)
                 fflush(stdout);
 
                 /* Get command line */
-                fgets(cmd, CMDLINE_MAX, stdin);
+                fgets(x1.input, CMDLINE_MAX, stdin); 	
                 
-	
+
                 /* Print command line if stdin is not provided by terminal */
                 if (!isatty(STDIN_FILENO)) {
-                        printf("%s", cmd);
+                        printf("%s", x1.input);
                         fflush(stdout);
                 }
 
                 /* Remove trailing newline from command line */
-                nl = strchr(cmd, '\n');
+                nl = strchr(x1.input,'\n');
                 if (nl)
                         *nl = '\0';
                 
+		strcpy(line, x1.input); 
+		parse_cmd(&x1,x1.input);
+
+
 		  /* Builtin command */
-                if (!strcmp(cmd, "exit")) {
+                if (!strcmp(x1.input, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
 
-
-
 		pid = fork();
                 if(pid ==0){
                  /*child*/
-                execvp(cmd,args);
+                execvp(x1.cmd,x1.args);
                 perror("execvp");
                 exit(1);
                 } else if (pid >0){ 
@@ -55,11 +94,13 @@ int main(void)
 		int status; 
 		waitpid(pid,&status,0); 
 	        retval = WEXITSTATUS(status);	
-                }
+                }  
 
+
+		
                 /* Regular command */
                 fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
+                        line, retval);
         }
 
         return EXIT_SUCCESS;
